ab7f05bf59705aa03bba06bb03152beb1aab3e41
Brian Thiaka <thiakabrian@gmail.com>
2026-06-04 11:43:39 +0300

    added NotificationsConfig

--- src/main/java/com/nuvemite/cms/notifications/config/NotificationsConfig.java
+++ src/main/java/com/nuvemite/cms/notifications/config/NotificationsConfig.java
@@ -1,8 +1,17 @@
 package com.nuvemite.cms.notifications.config;
 
+import com.nuvemite.cms.notifications.service.NotificationEventService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(NotificationsMailProperties.class)
-public class NotificationsConfig {}
+public class NotificationsConfig {
+
+    @Bean
+    ApplicationRunner notificationEventDefinitionSynchronizer(NotificationEventService eventService) {
+        return args -> eventService.synchronizeSystemDefinitions();
+    }
+}
